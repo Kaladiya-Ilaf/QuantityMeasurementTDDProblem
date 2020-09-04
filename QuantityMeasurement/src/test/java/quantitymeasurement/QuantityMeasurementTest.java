1e6983c31d294942256ce0f639858414afa59ca1
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-09-04 22:48:29 +0530

    Added : Test and code to check equality of 2 given length with one of the value as null.

--- QuantityMeasurement/src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ QuantityMeasurement/src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -5,16 +5,23 @@ import org.junit.Test;
 
 public class QuantityMeasurementTest {
     @Test
-    public void given2Lengths_whenOfTypeFeetAndBothHasValueZero_shouldReturnTrue() {
+    public void given2Lengths_whenOfTypeFeetAndBothHasValueZero_shouldBeEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
     }
 
     @Test
-    public void given2Lengths_whenOfTypeFeetAndBothHasValueZeroAndOne_shouldReturnTrue() {
+    public void given2Lengths_whenOfTypeFeetAndBothHasValueZeroAndOne_shouldBeUnequal() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    public void  given2Lengths_whenOfTypeFeetAndBothHasValueZeroAndNull_shouldBeUnequal() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = null;
         Assert.assertNotEquals(feet1, feet2);
     }
 }

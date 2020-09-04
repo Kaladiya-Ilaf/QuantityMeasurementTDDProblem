249b6af80149d165ea2c1244d8179d1a8668dc42
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-09-04 23:22:09 +0530

    Added : Test and code to check type equality of 2 given length is same and different .

--- QuantityMeasurement/src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ QuantityMeasurement/src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -54,4 +54,18 @@ public class QuantityMeasurementTest {
         Feet feet2 = new Feet(1.0);
         Assert.assertNotSame(feet1, feet2);
     }
+
+    @Test
+    public void given2Lengths_whenHasSameTypes_shouldBeEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertEquals(feet1.getClass(), feet2.getClass());
+    }
+
+    @Test
+    public void given2Lengths_whenHasDifferentTypes_shouldBeUnequal() {
+        Feet feet1 = new Feet(0.0);
+        Integer int1 = 0;
+        Assert.assertNotEquals(feet1.getClass(), int1.getClass());
+    }
 }

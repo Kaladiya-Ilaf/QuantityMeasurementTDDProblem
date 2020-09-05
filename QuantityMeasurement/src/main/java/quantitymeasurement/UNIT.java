85e6aba784bf8b955734aa920ff3b165ccf6cc85
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-09-05 08:36:59 +0530

    Modified : Enum to accommodate centimeter

--- QuantityMeasurement/src/main/java/quantitymeasurement/UNIT.java
+++ QuantityMeasurement/src/main/java/quantitymeasurement/UNIT.java
@@ -3,8 +3,8 @@ package quantitymeasurement;
 enum Unit {
     FEET(12.0),
     INCH(1.0),
-    YARD(36.0);
-
+    YARD(36.0),
+    CENTIMETER(0.4) ;
     private final double baseUnit;
 
     Unit (double baseUnit) {

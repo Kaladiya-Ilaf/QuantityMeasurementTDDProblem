277028c15d5abdea1b9bf3e2c11cffacf4b3310a
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-09-05 13:44:17 +0530

    Refacored : Add method to avoid adding temperature values

--- QuantityMeasurement/src/main/java/quantitymeasurement/AddQuantity.java
+++ QuantityMeasurement/src/main/java/quantitymeasurement/AddQuantity.java
@@ -2,7 +2,7 @@ package quantitymeasurement;
 
 public class AddQuantity {
     public double AddQuantities(QuantityMeasurement comparator1, QuantityMeasurement comparator2) {
-        if (comparator1.unit.unitType.equals(comparator2.unit.unitType)) {
+        if (comparator1.unit.unitType.equals(comparator2.unit.unitType) && comparator1.unit.unitType != UnitType.TEMPERATURE) {
             return (comparator1.value * comparator1.unit.baseUnit) + (comparator2.value * comparator2.unit.baseUnit);
         }
         return 0;
